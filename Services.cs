4a89c789dc9d88c2f7f3d805459849deaefb6f4e
pastaya <contact@pastaya.net>
2025-12-26 10:44:30 +0100

    refactor: use 2 space indent

--- Services.cs
+++ Services.cs
@@ -1,13 +1,13 @@
 namespace NetroCorp.Lookup;
 public static class Services {
-    public static string? WhatIs(string service) {
-        if (string.IsNullOrWhiteSpace(service)) {
-            return null;
-        }
-        return service.Trim().ToLowerInvariant() switch {
-            "netrohost" => "NetroHost is a hosting service for everyone.",
-            "thecodingbot" => "TheCodingBot is a discord bot made by TheCodingGuy. It's in over a hundred servers!",
-            _ => null,
-        };
+  public static string? WhatIs(string service) {
+    if (string.IsNullOrWhiteSpace(service)) {
+      return null;
     }
+    return service.Trim().ToLowerInvariant() switch {
+      "netrohost" => "NetroHost is a hosting service for everyone.",
+      "thecodingbot" => "TheCodingBot is a discord bot made by TheCodingGuy. It's in over a hundred servers!",
+      _ => null,
+    };
+  }
 }
